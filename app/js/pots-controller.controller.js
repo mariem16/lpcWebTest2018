022e8bda8fc79e760109dc43156a7f781660f18e
lepotcommun <30501704+lepotcommun@users.noreply.github.com>
2018-10-23 16:43:39 +0200

    Update pots-controller.controller.js

--- app/js/pots-controller.controller.js
+++ app/js/pots-controller.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('LpcWebTest2017')
+angular.module('LpcWebTest2018')
     .controller('PotsController',['$scope','PotsService',function($scope,PotsService){
         //TODO
     }]);
