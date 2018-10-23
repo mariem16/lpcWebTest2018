ad0f01fd1803881c99a6a3b8c164b9b37f728686
lepotcommun <30501704+lepotcommun@users.noreply.github.com>
2018-10-23 16:43:50 +0200

    Update pots-service.service.js

--- app/js/pots-service.service.js
+++ app/js/pots-service.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('LpcWebTest2017')
+angular.module('LpcWebTest2018')
     .service('PotsService',['$http','RESOURCES',function($http,RESOURCES){
         var getImpl = function(){
             //TODO
